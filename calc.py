68330dcdbbd60d8a6f386476fc702d3cde390514
Python <dumwebtecnologia@gmail.com>
2023-11-28 11:47:44 -0300

    commit calculadora de mas

---
+++ calc.py
@@ -0,0 +1,16 @@
+# Calculadora Simples
+
+# Apresentação
+
+print('\n\t\t -- Calculadora --\n')
+
+# Entradas
+n1 = int(input('informe n1'))
+n2 = int(input('informe n2'))
+
+# Processamento
+total = n1 + n2
+
+# Saida
+print (f'{n1} + {n2}) = {total}')
+
